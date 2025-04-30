7de7cf0093f003f9ef9649566d671ee7f73d9096
Polka37 <marcel.klo.2006@gmail.com>
2025-04-30 22:05:18 +0200

    Add files via upload

---
+++ categories/equipment-categories.lua
@@ -0,0 +1,5 @@
+data:extend({
+  { type = "equipment-category", name = "kr-vehicle" },
+  { type = "equipment-category", name = "kr-vehicle-motor" },
+  { type = "equipment-category", name = "kr-vehicle-roboport" },
+})
