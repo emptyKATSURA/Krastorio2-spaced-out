62c028e1f886ade218e01817d09ded4e3b04ccd8
Polka37 <marcel.klo.2006@gmail.com>
2025-04-30 22:23:14 +0200

    Delete types.lua

--- types.lua
+++
@@ -1,9 +0,0 @@
---- @meta
-
---- @alias EntityBuiltEvent EventData.on_built_entity|EventData.on_robot_built_entity|EventData.on_space_platform_built_entity|EventData.on_entity_cloned|EventData.script_raised_built|EventData.script_raised_revive
---- @alias EntityDestroyedEvent EventData.on_player_mined_entity|EventData.on_robot_mined_entity|EventData.on_space_platform_mined_entity|EventData.on_entity_died|EventData.script_raised_destroy
-
---- @alias PlayerIndex uint
---- @alias UnitNumber uint
-
---- @alias k2.PrototypeWithIcons data.AchievementPrototype|data.AmmoCategory|data.AsteroidChunkPrototype|data.EntityPrototype|data.FluidPrototype|data.ItemGroup|data.ItemPrototype|data.QualityPrototype|data.RecipePrototype|data.ShortcutPrototype|data.SpaceConnectionPrototype|data.SpaceLocationPrototype|data.TechnologyPrototype|data.TilePrototype|data.TipsAndTricksItem|data.VirtualSignalPrototype
