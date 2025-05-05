4577b4fe3d229bb85557d28fc18ca792740c51c7
Polka37 <marcel.klo.2006@gmail.com>
2025-05-05 17:07:01 +0200

    Delete prototypes directory

--- prototypes/categories/fuel-categories.lua
+++
@@ -1,5 +0,0 @@
-data:extend({
-  { type = "fuel-category", name = "kr-antimatter-fuel" },
-  { type = "fuel-category", name = "kr-fusion-fuel" },
-  { type = "fuel-category", name = "kr-vehicle-fuel" },
-})
--- prototypes/compatibility/aircraft.lua
+++
@@ -1,7 +0,0 @@
-if not mods["Aircraft"] then
-  return
-end
-
--- Remove obsolete items
-data.raw.recipe["aircraft-energy-shield"] = nil
-data.raw.technology["aircraft-energy-shield"] = nil
