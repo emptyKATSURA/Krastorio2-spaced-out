9841621cd8b074961c156596466f0a2fdc458051
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-05-21 12:14:46 +0200

    Adjust stack sizes for space age

--- prototypes/updates/adjust-stack-sizes.lua
+++ prototypes/updates/adjust-stack-sizes.lua
@@ -43,6 +43,41 @@ data.raw.item["uranium-ore"].stack_size = kr_stack_size_value
 data.raw.item["wood"].stack_size = kr_stack_size_value
 data.raw.tool[kr_optimization_tech_card_name].stack_size = 200
 
+-- Space Age
+
+data.raw.item["tree-seed"].stack_size = kr_stack_size_value
+data.raw.item["artificial-yumako-soil"].stack_size = kr_stack_size_value
+data.raw.item["artificial-jellynut-soil"].stack_size = kr_stack_size_value
+data.raw.item["overgrowth-yumako-soil"].stack_size = kr_stack_size_value
+data.raw.item["overgrowth-jellynut-soil"].stack_size = kr_stack_size_value
+data.raw.capsule["jellynut"].stack_size = kr_stack_size_value
+data.raw.item["jellynut-seed"].stack_size = kr_stack_size_value
+data.raw.capsule["jelly"].stack_size = kr_stack_size_value
+data.raw.capsule["yumako"].stack_size = kr_stack_size_value
+data.raw.item["yumako-seed"].stack_size = kr_stack_size_value
+data.raw.capsule["yumako-mash"].stack_size = kr_stack_size_value
+data.raw.item["nutrients"].stack_size = kr_stack_size_value
+data.raw.item["iron-bacteria"].stack_size = kr_stack_size_value
+data.raw.item["copper-bacteria"].stack_size = kr_stack_size_value
+data.raw.capsule["bioflux"].stack_size = kr_stack_size_value
+data.raw.item["carbon"].stack_size = kr_stack_size_value
+data.raw.item["carbon-fiber"].stack_size = kr_stack_size_value
+data.raw.item["calcite"].stack_size = kr_stack_size_value
+data.raw.item["tungsten-ore"].stack_size = kr_stack_size_value
+data.raw.item["tungsten-carbide"].stack_size = kr_stack_size_value
+data.raw.item["tungsten-plate"].stack_size = kr_stack_size_value
+data.raw.item["holmium-ore"].stack_size = kr_stack_size_value
+data.raw.item["holmium-plate"].stack_size = kr_stack_size_value
+data.raw.item["superconductor"].stack_size = kr_stack_size_value
+data.raw.item["supercapacitor"].stack_size = kr_stack_size_value
+data.raw.item["ice"].stack_size = kr_stack_size_value
+data.raw.item["lithium"].stack_size = kr_stack_size_value
+data.raw.item["lithium-plate"].stack_size = kr_stack_size_value
+data.raw.item["quantum-processor"].stack_size = kr_stack_size_value
+data.raw.item["foundation"].stack_size = kr_stack_size_value
+data.raw.item["ice-platform"].stack_size = kr_stack_size_value
+data.raw.item["foundation"].stack_size = kr_stack_size_value
+
 -- K2
 
 data.raw.item["kr-advanced-fuel"].stack_size = kr_stack_size_value
