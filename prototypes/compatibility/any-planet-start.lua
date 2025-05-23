eda6e33054de7f14ed1b94e204e3b84f7c15f710
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-05-23 22:13:25 +0200

    Added compatibility for any planet start

---
+++ prototypes/compatibility/any-planet-start.lua
@@ -0,0 +1,32 @@
+if not mods["any-planet-start"] then
+  return
+end
+
+local data_util = require("data-util")
+
+if settings.startup["aps-planet"].value == "gleba" then
+    data.raw.technology["agriculture"].research_trigger = {
+        type = "craft-item",
+        item = "landfill",
+        count = 1
+        }
+    data_util.remove_recipe_unlock("biochamber", "kr-bio-processing-circuit")
+    data_util.add_recipe_unlock("processing-unit", "kr-bio-processing-circuit")
+end
+
+if settings.startup["aps-planet"].value == "vulcanus" then
+    data_util.remove_prerequisite("solar-energy", "kr-silicon-processing")
+    data_util.add_prerequisite("solar-energy", "kr-stone-processing")
+    data_util.remove_ingredient("solar-panel", "silicon")
+    data_util.add_or_replace_ingredient(
+        "solar-panel",
+        "kr-silicon",
+    { type = "item", name = "copper-plate", amount = 5 } )
+    data_util.remove_recipe_unlock("tungsten-carbide", "kr-carbide-processing-circuit")
+    data_util.add_recipe_unlock("processing-unit", "kr-carbide-processing-circuit")
+end
+
+if settings.startup["aps-planet"].value == "fulgora" then
+    data_util.remove_prerequisite("advanced-circuit", "kr-silicon-processing")
+    data_util.remove_prerequisite("steam-power", "fluid-handling")
+end
