2e2a98a258e68a10dba6abaa8061f81f466241fa
Mac <baghdady92@gmail.com>
2025-05-29 22:32:47 +0300

    Add compatibility fixes for Maraxsis mod, including hiding items and adjusting technology effects

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -163,6 +163,94 @@ if mods["aai-industry"] then
     end
   end
 end
+if mods["maraxsis"] then
+    log("K2-Maraxsis compat: Applying compatibility fixes for Maraxsis.")
+
+    -- Hide Maraxsis's "sand" item if it's specifically Maraxsis's version.
+    -- The generic process_ingredients function (called if AAI is active) will handle converting "sand"
+    -- ingredients in recipes to "kr-sand".
+    if data.raw.item["sand"] then
+        local item_sand_data = data.raw.item["sand"]
+        if item_sand_data.icon and type(item_sand_data.icon) == "string" and string.find(item_sand_data.icon, "__maraxsis__", 1, true) then
+            log("K2-Maraxsis compat: Hiding Maraxsis 'sand' item.")
+            item_sand_data.hidden = true
+            if item_sand_data.subgroup and data.raw["item-subgroup"] and data.raw["item-subgroup"][item_sand_data.subgroup] then
+                local subgroup_items = data.raw["item-subgroup"][item_sand_data.subgroup].order
+                if subgroup_items then
+                    for i = #subgroup_items, 1, -1 do
+                        if subgroup_items[i] == "sand" then -- Assuming Maraxsis item name is "sand"
+                            table.remove(subgroup_items, i)
+                            log("K2-Maraxsis compat: Removed Maraxsis 'sand' item from subgroup order: " .. item_sand_data.subgroup)
+                            break
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    -- Fix maraxsis-glass-productivity technology error.
+    -- This error occurs specifically when AAI Industry is also present, because the K2-AAI fix
+    -- nils the generic "glass" recipe, which maraxsis-glass-productivity then tries to reference.
+    if mods["aai-industry"] then
+        local maraxsis_tech_name = "maraxsis-glass-productivity"
+        if data.raw.technology[maraxsis_tech_name] then
+            local tech_data = data.raw.technology[maraxsis_tech_name]
+            if tech_data.effects then
+                log("K2-Maraxsis compat: Checking '" .. maraxsis_tech_name .. "' technology effects (due to AAI Industry presence).")
+                local effect_removed_count = 0
+                for i = #tech_data.effects, 1, -1 do
+                    local effect = tech_data.effects[i]
+                    if effect.type == "change-recipe-productivity" and effect.recipe == "glass" then
+                        log("K2-Maraxsis compat: Removing problematic 'glass' recipe productivity effect (entry #" .. i .. ") from '" .. maraxsis_tech_name .. "'.")
+                        table.remove(tech_data.effects, i)
+                        effect_removed_count = effect_removed_count + 1
+                    end
+                end
+                if effect_removed_count > 0 then
+                    log("K2-Maraxsis compat: Removed " .. effect_removed_count .. " problematic 'glass' recipe effect(s) from '" .. maraxsis_tech_name .. "'.")
+                else
+                    log("K2-Maraxsis compat: No problematic 'glass' recipe effect found to remove in '" .. maraxsis_tech_name .. "' (when checking for AAI conflict).")
+                end
+            end
+        else
+            log("K2-Maraxsis compat: Technology '" .. maraxsis_tech_name .. "' not found (when checking for AAI conflict).")
+        end
+    end
+end
+-- Ensure electric-mining-drill and its K2 upgrade target have the same collision mask
+local base_drill_name = "electric-mining-drill"
+local k2_upgrade_drill_name = "kr-electric-mining-drill-mk2"
+
+if data.raw["mining-drill"] and data.raw["mining-drill"][base_drill_name] and data.raw["mining-drill"][k2_upgrade_drill_name] then
+    local base_drill = data.raw["mining-drill"][base_drill_name]
+    local k2_upgrade_drill = data.raw["mining-drill"][k2_upgrade_drill_name]
+
+    -- If the base drill is set to upgrade to the K2 drill, ensure collision masks match.
+    if base_drill.next_upgrade and base_drill.next_upgrade == k2_upgrade_drill_name then
+        if k2_upgrade_drill.collision_mask then
+            -- The error indicates they are not the same. Force K2's mask onto the base drill.
+            -- If Maraxsis is active, it's known to add "maraxsis_dome_collision_mask" to the electric-mining-drill's layers.
+            -- To ensure the upgrade remains valid, the kr-electric-mining-drill-mk2's collision_mask must also account for this layer.
+            if mods["maraxsis"] then
+                log("K2-Maraxsis compat: Aligning '" .. k2_upgrade_drill_name .. "' collision_mask to include 'maraxsis_dome_collision_mask' for upgrade from '"..base_drill_name.."'.")
+                k2_upgrade_drill.collision_mask = k2_upgrade_drill.collision_mask or {layers={}} -- Ensure table exists
+                k2_upgrade_drill.collision_mask.layers = k2_upgrade_drill.collision_mask.layers or {} -- Ensure layers table exists
+                k2_upgrade_drill.collision_mask.layers["maraxsis_dome_collision_mask"] = true -- Add/ensure Maraxsis layer is present
+                log("K2-Maraxsis compat: '" .. k2_upgrade_drill_name .. "' collision_mask.layers.maraxsis_dome_collision_mask set to true.")
+            end
+
+            -- Now, force the base drill's mask to be identical to the (potentially Maraxsis-compat-adjusted) K2 upgrade drill's mask.
+            log("K2 compat: Forcing collision_mask of '" .. base_drill_name .. "' to match final collision_mask of next_upgrade target '" .. k2_upgrade_drill_name .. "'.")
+            base_drill.collision_mask = table.deepcopy(k2_upgrade_drill.collision_mask)
+            log("K2 compat: collision_mask for '" .. base_drill_name .. "' updated.")
+        else
+            log("K2 compat: K2 upgrade drill '" .. k2_upgrade_drill_name .. "' has no collision_mask. Cannot align for '" .. base_drill_name .. "'.")
+        end
+    end
+else
+    log("K2 compat: Mining drill '" .. base_drill_name .. "' or '" .. k2_upgrade_drill_name .. "' not found. Skipping collision_mask alignment for upgrade path.")
+end
 
 if settings.startup["kr-steel-pipes-need-pumps"].value then
   return
