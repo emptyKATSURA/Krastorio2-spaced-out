9cc4d2809f935f77167bbd00173ecf1db0ba27f2
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-06-02 22:55:41 +0200

    Fix scrap recycling didn't yield electronic circuits

--- prototypes/updates/base/recipes.lua
+++ prototypes/updates/base/recipes.lua
@@ -700,8 +700,21 @@ data.raw.recipe["casting-steel"].icons = {
       { icon = "__Krastorio2Assets__/icons/items/steel-plate.png" },
       { icon = "__space-age__/graphics/icons/fluid/molten-iron.png", scale = 0.33, shift = { 8, -8 }, },
 }
-
-data_util.add_or_replace_product( "scrap-recycling", "electronic-circuit", { type = "item", name = "electronic-circuit", amount = 1, probability = 0.1, } )
+data.raw.recipe["scrap-recycling"].results = {
+      {type = "item", name = "iron-gear-wheel",        amount = 1, probability = 0.20, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "electronic-circuit",     amount = 1, probability = 0.10, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "solid-fuel",             amount = 1, probability = 0.07, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "concrete",               amount = 1, probability = 0.06, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "ice",                    amount = 1, probability = 0.05, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "steel-plate",            amount = 1, probability = 0.04, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "battery",                amount = 1, probability = 0.04, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "stone",                  amount = 1, probability = 0.04, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "advanced-circuit",       amount = 1, probability = 0.03, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "copper-cable",           amount = 1, probability = 0.03, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "processing-unit",        amount = 1, probability = 0.02, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "low-density-structure",  amount = 1, probability = 0.01, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "holmium-ore",            amount = 1, probability = 0.01, show_details_in_recipe_tooltip = false},
+}
 
 -- Science packs
 
