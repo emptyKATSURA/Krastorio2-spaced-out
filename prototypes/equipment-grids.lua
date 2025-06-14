df403d862edef9fca8d553561221bbba48f56b2e
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-06-14 22:55:09 +0200

    cleanup

--- prototypes/equipment-grids.lua
+++ prototypes/equipment-grids.lua
@@ -41,18 +41,4 @@ data:extend({
     height = 16,
     equipment_categories = { "kr-vehicle", "kr-vehicle-roboport" },
   },
-  {
-    type = "equipment-grid",
-    name = "kr-mk3-armor-grid",
-    width = 10,
-    height = 10,
-    equipment_categories = { "armor" },
-  },
-  {
-    type = "equipment-grid",
-    name = "kr-mk4-armor-grid",
-    width = 12,
-    height = 12,
-    equipment_categories = { "armor" },
-  },
 })
