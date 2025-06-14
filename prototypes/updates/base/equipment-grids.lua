218c08084ba662b30477e420fbe0583826f91ff9
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-06-14 22:54:40 +0200

    Buff power armor mk2

--- prototypes/updates/base/equipment-grids.lua
+++ prototypes/updates/base/equipment-grids.lua
@@ -1,5 +1,5 @@
-data.raw["equipment-grid"]["large-equipment-grid"].height = 8
-data.raw["equipment-grid"]["large-equipment-grid"].width = 8
+data.raw["equipment-grid"]["large-equipment-grid"].height = 10
+data.raw["equipment-grid"]["large-equipment-grid"].width = 10
 data.raw["equipment-grid"]["medium-equipment-grid"].height = 6
 data.raw["equipment-grid"]["medium-equipment-grid"].width = 6
 data.raw["equipment-grid"]["small-equipment-grid"].height = 4
