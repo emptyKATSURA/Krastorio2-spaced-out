78c432035a566ba59a0802730c8011801f5e0ac6
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-08-03 18:36:14 +0200

    Add support for Castra/Age of industry
    Fixed lack of ammo added by weapons-realistic.lua in forge/armory

---
+++ prototypes/compatibility/castra.lua
@@ -0,0 +1,20 @@
+if not mods["castra"] and not mods["Age-of-Production"] then
+  return
+end
+
+function add_grop(group)
+    data.raw.recipe["kr-rifle-magazine"].category = group
+    data.raw.recipe["kr-armor-piercing-rifle-magazine"].category = group
+    data.raw.recipe["kr-uranium-rifle-magazine"].category = group
+    data.raw.recipe["kr-imersite-rifle-magazine"].category = group
+    data.raw.recipe["kr-anti-materiel-rifle-magazine"].category = group
+    data.raw.recipe["kr-armor-piercing-anti-materiel-rifle-magazine"].category = group
+    data.raw.recipe["kr-uranium-anti-materiel-rifle-magazine"].category = group
+    data.raw.recipe["kr-imersite-anti-materiel-rifle-magazine"].category = group
+end
+
+if settings.startup["kr-realistic-weapons"].value then
+    if mods["castra"] and not mods["Age-of-Production"] then add_grop("castra-crafting") end
+    if not mods["castra"] and mods["Age-of-Production"] then add_grop("ammunition") end
+    if mods["castra"] and mods["Age-of-Production"] then add_grop("castra-ammunition-or-crafting") end
+end
