6ae5e976a9b3a7247d980639d3cc4e11e96ca842
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-08-09 13:09:26 +0200

    Re-add stone and holmium to recycling

--- prototypes/updates/base/recipes.lua
+++ prototypes/updates/base/recipes.lua
@@ -721,12 +721,14 @@ data.raw.recipe["scrap-recycling"].results = {
       {type = "item", name = "concrete",               amount = 1, probability = 0.06, show_details_in_recipe_tooltip = false},
       {type = "item", name = "ice",                    amount = 1, probability = 0.05, show_details_in_recipe_tooltip = false},
       {type = "item", name = "steel-plate",            amount = 1, probability = 0.04, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "stone",                  amount = 1, probability = 0.04, show_details_in_recipe_tooltip = false},
       {type = "item", name = "battery",                amount = 1, probability = 0.04, show_details_in_recipe_tooltip = false},
       {type = "item", name = "kr-electronic-components",amount = 1, probability = 0.04, show_details_in_recipe_tooltip = false},
       {type = "item", name = "advanced-circuit",       amount = 1, probability = 0.03, show_details_in_recipe_tooltip = false},
       {type = "item", name = "copper-cable",           amount = 1, probability = 0.03, show_details_in_recipe_tooltip = false},
       {type = "item", name = "processing-unit",        amount = 1, probability = 0.02, show_details_in_recipe_tooltip = false},
       {type = "item", name = "low-density-structure",  amount = 1, probability = 0.01, show_details_in_recipe_tooltip = false},
+      {type = "item", name = "holmium-ore",            amount = 1, probability = 0.01, show_details_in_recipe_tooltip = false},
 }
 
 -- Science packs
